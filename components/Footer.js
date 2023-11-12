9ab1cd3a8ca0048b1ab849b7c33edd2f5e083346
AaduMaadu <aadeshsenthil@gmail.com>
2023-11-12 16:22:04 -0600

    modified:   components/Footer.js 	modified:   package-lock.json 	modified:   pages/_app.js 	modified:   pages/index.js 	new file:   public/profile-pic.jpg 	new file:   styles/styles.css

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,9 @@ export default function Footer() {
       <footer className={styles.footer}>
         <img src="/Makerspace.png" alt="Makerspace Logo" className={styles.logo} />
         {/* Need to be able to write some text on the right of the Makerspace Logo */}
-        <p> @Aadesh Senthilkumar</p>
+        <p> 
+          @<a href="https://wiki.utd.ms/" target="_blank" >Makerspace</a>❤
+        </p>
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,39 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  const FileSystemHandle = (event) => {
-    const file = event.target.files[0];
-    console.log('Selected file:', file);
+  const redirectToGitHub = () => {
+    window.open("https:/github.com/AaduMaadu/MS-Resume", "_blank");
   };
+  // const FileSystemHandle = (event) => {
+  //   const file = event.target.files[0];
+  //   console.log('Selected file:', file);
+  // };
   
   return (
     <div className="container">
       <Head>
-        <title>Resume Fabricator</title>
+        <title>Welcome to my Website</title>
         <link rel="icon" href="/Makerspace.png" />
+        {/* <link rel="stylesheet" href="/styles.css" /> */}
       </Head>
 
       <main>
-        <Header title="Start by uploading your resume" />
+        <Header title="About me" />
+        <p>
+          Hi, my name is Aadesh Senthilkumar
+        </p>
         <p className="description">
-          {/* Create button to upload pdf */}
-          <input type="file" accept=".pdf" onChange={FileSystemHandle}/>
-          <code>Upload PDF</code>
+          <div className="image-profile">
+            <img src="./profile-pic.jpg" alt="Aadesh" height="250" width="200"></img>
+          </div>
+        
+          {/* <label htmlFor="file" className="custom-button" href="https://github.com/AaduMaadu/MS-Resume">Github</label> */}
+          {/* <input type="file" accept=".pdf" id="file" className="file-input" onChange={FileSystemHandle}/> */}
+          <p>
+            <button onClick={redirectToGitHub} className="custom-button">Github</button>
+            <button onClick={redirectToGitHub} className="custom-button">LinkedIn</button>
+            <button onClick={redirectToGitHub} className="custom-button">Minecraft</button>
+          </p>
         </p>
       </main>
 
